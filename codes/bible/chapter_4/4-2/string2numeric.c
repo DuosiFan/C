1576beb751243fe1828afb14e9e1ed78f87247c5
Mitch <duosifan@gmail.com>
2022-03-21 22:43:54 -0400

    clean

--- codes/bible/chapter_4/4-2/string2numeric.c
+++ codes/bible/chapter_4/4-2/string2numeric.c
@@ -7,17 +7,17 @@ double atof(char s[]);
 
 int main(void)
 {
-  char s[] = ".45111";
+  char s[] = "1.2e+3";
   double val = atof(s);
 
-  printf("%7.5f\n", val);
+  printf("%f\n", val);
   return 0;
 }
 
 /* atof: conver string s to double */
 double atof(char s[]) {
   double val, power;
-  int i, sign;
+  int i, sign, expsign, exp;
 
   for (i = 0; isspace(s[i]); i++); /* skip blanks */
 
@@ -30,15 +30,34 @@ double atof(char s[]) {
   for (val = 0.0; isdigit(s[i]); i++)
     val = BASE*val + (s[i] - '0');
 
-  /* check the decimal point exists */
+  /* check the decimal point exists and read fraction */
   if (s[i] == '.')
     i++;
-
-  /* read fraction part */
   for (power = 1.0; isdigit(s[i]); i++) {
     val = BASE*val + (s[i] - '0');
     power *= BASE;
   }
+
+  /* exponent part */  
+  if (s[i] == 'e' || s[i] == 'E')
+    i++;
+  expsign = (s[i] == '-') ? -1 : 1;
+  if (s[i] == '-' || s[i] == '+')
+    i++;
+  for (exp = 0; isdigit(s[i]); i++)
+    exp = BASE*exp + (s[i] - '0');
+
+  /* modify power based on the exp */
+  if (expsign > 0)
+    while (exp > 0) {
+      power /= BASE;
+      exp--;
+    }
+  else
+    while (exp < 0) {
+      power *= BASE;
+      exp--;
+    }
   
   return  sign*val/power;
 }
