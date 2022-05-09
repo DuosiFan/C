79bf3881e9ff2239db7a486631cc16c1659ce0e6
Mitch <duosifan@gmail.com>
2022-05-09 00:43:10 -0400

    remove course codes

--- codes/learn2prog/08_testing/execute.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-
-./isPrime-broken3 $1
-
-
-echo Correct Answer
-./isPrime-correct $1
--- codes/learn2prog/23_power_rec/power.c
+++
@@ -1,8 +0,0 @@
-
-unsigned power(unsigned x, unsigned y) {
-  /* base case */
-  if (y == 0U)
-    return 1U;
-
-  return x * power(x, y - 1);
-}
